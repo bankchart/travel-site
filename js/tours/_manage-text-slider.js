710ca9e0d0a09cfe2bcd9d3f1e7ba0d0a957c8ff
bankchart <mrbankshart@gmail.com>
2016-04-26 00:48:57 +0700

    add loading.gif to beforeSend query slider ajax

--- js/tours/_manage-text-slider.js
+++ js/tours/_manage-text-slider.js
@@ -1,5 +1,5 @@
 $(function(){
-    $('.manage-label').on('click', function(){
+    $('body').on('click', '.manage-label', function(){
         location.href='Update-Text-Slider-Form/' + this.id;
     });
 
@@ -29,20 +29,28 @@ $(function(){
             type: 'post',
             dataType: 'json',
             beforeSend: function(){
-            //    console.log('show-records : ' + $('#show-records').val());
-            //    console.log('show-page : ' + $('#show-page').val());
+                var loading = '<tr>';
+                loading += '<td colspan="5" style="text-align: center">';
+                loading += '<img width="20" src="../images/loading.gif"/>';
+                loading += '</td>';
+                loading += '</tr>';
+                elm_update.html(loading);
             },
             success: function(data){
-            //    console.log('response === >>>' + data.slider_content_partial);
-                elm_update.html(data.slider_content_partial);
+                setTimeout(function(){
+                    if(data.slider_content_partial != ''){
+                        elm_update.html(data.slider_content_partial);
+                    }else{
+                        elm_update.html('<tr><td colspan="5" style="text-align: center">Slider empty.</td></tr>');
+                    }
+                }, 500);
                 $('#show-page').html(data.selectPage);
                 $('#record-info').html(data.record_info);
             },
             error: function(responseText){},
             complete: function(data){
-            //    console.log(data);
-                if(data.responseJSON.slider_content_partial == "")
-                    elm_update.html('<tr><td colspan="5" style="text-align: center">Slider empty.</td></tr>');
+                // if(data.responseJSON.slider_content_partial == "")
+                //     elm_update.html('<tr><td colspan="5" style="text-align: center">Slider empty.</td></tr>');
             }
         });
     }
